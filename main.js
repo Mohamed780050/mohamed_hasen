84db83514bd50d51a6b3bc1540d657329f6fedca
Mohamed780050 <mohamed39omer@gmail.com>
2025-12-13 20:57:59 +0200

    added main.js file with the logic to change videos

---
+++ main.js
@@ -0,0 +1,24 @@
+const videoFrame = document.getElementById("video");
+const nextButton = document.getElementById("nextButton");
+const prevButton = document.getElementById("prevButton");
+const videos = [
+  "https://www.youtube.com/embed/8UjX_VIrYrc?si=15l9W07H6bOs_HIx",
+  "https://www.youtube.com/embed/c7Hge1cFdhw?si=zRtof8yasbujb_WE",
+  "https://www.youtube.com/embed/huF_MLkdjrE",
+];
+let counter = 0;
+
+nextButton.onclick = () => {
+  if (counter === 2) return;
+  counter++;
+  videoFrame.src = videos[counter];
+  console.log(counter);
+};
+prevButton.onclick = () => {
+  console.log(counter);
+
+  if (counter === 0) return;
+
+  counter--;
+  videoFrame.src = videos[counter];
+};
