d5903d429109d72bf400e79049452397b2b64155
Mohamed780050 <mohamed39omer@gmail.com>
2025-12-14 14:56:11 +0200

    hashed the code from github

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const videoFrame = document.getElementById("video");
-const nextButton = document.getElementById("nextButton");
-const prevButton = document.getElementById("prevButton");
-const videos = [
-  "https://www.youtube.com/embed/8UjX_VIrYrc?si=15l9W07H6bOs_HIx",
-  "https://www.youtube.com/embed/c7Hge1cFdhw?si=zRtof8yasbujb_WE",
-  "https://www.youtube.com/embed/huF_MLkdjrE",
-];
-let counter = 0;
+// const videoFrame = document.getElementById("video");
+// const nextButton = document.getElementById("nextButton");
+// const prevButton = document.getElementById("prevButton");
+// const videos = [
+//   "https://www.youtube.com/embed/8UjX_VIrYrc?si=15l9W07H6bOs_HIx",
+//   "https://www.youtube.com/embed/c7Hge1cFdhw?si=zRtof8yasbujb_WE",
+//   "https://www.youtube.com/embed/huF_MLkdjrE",
+// ];
+// let counter = 0;
 
-nextButton.onclick = () => {
-  if (counter === 2) return;
-  counter++;
-  videoFrame.src = videos[counter];
-  console.log(counter);
-};
-prevButton.onclick = () => {
-  console.log(counter);
+// nextButton.onclick = () => {
+//   if (counter === 2) return;
+//   counter++;
+//   videoFrame.src = videos[counter];
+//   console.log(counter);
+// };
+// prevButton.onclick = () => {
+//   console.log(counter);
 
-  if (counter === 0) return;
+//   if (counter === 0) return;
 
-  counter--;
-  videoFrame.src = videos[counter];
-};
+//   counter--;
+//   videoFrame.src = videos[counter];
+// };
